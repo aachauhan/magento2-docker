63b3f044fa14c57ddf7465b6f1168baf6edcecd8
Aditya Chauhan <chauhan.adie@gmail.com>
2024-01-25 17:01:39 -0700

    Added a new module to add custom product attribute

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-Project code is in the src/ folder
-Start the project with running bin/start
-See the frontend at https://magento.test/
-Stop the project with bin/stop
+1. Project code is in the src/ folder
+
+2. Start the project with running bin/start
+
+3. See the frontend at https://magento.test/
+
+4. Stop the project with bin/stop
---
+++ src/app/code/AdiDev/CustomProductAttribute/setup/InstallData.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AdiDev\CustomProductAttribute\Setup;
+
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+    private $eavSetupFactory;
+
+    public function __construct(EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+
+        $eavSetup->addAttribute(
+            \Magento\Catalog\Model\Product::ENTITY,
+            'manufacturer_name',
+            [
+                'type' => 'varchar',
+                'label' => 'Manufacturer Name',
+                'input' => 'text',
+                'required' => false,
+                'visible' => true,
+                'user_defined' => true,
+                'searchable' => true,
+                'filterable' => true,
+                'comparable' => true,
+                'visible_on_front' => true,
+                'used_in_product_listing' => true,
+                'unique' => false,
+                'group' => 'General'
+            ]
+        );
+
+        $setup->endSetup();
+    }
+}

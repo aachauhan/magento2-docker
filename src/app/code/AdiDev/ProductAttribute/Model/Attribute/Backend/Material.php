cd3aae30f82afbbbc442634817d39beff51e8b87
Aditya Chauhan <chauhan.adie@gmail.com>
2024-01-29 17:39:25 -0700

    Adding another custom product attribute

---
+++ src/app/code/AdiDev/ProductAttribute/Model/Attribute/Backend/Material.php
@@ -0,0 +1,24 @@
+<?php
+declare(strict_types = 1);
+
+namespace AdiDev\ProductAttribute\Model\Attribute\Backend;
+
+use Magento\Catalog\Model\Product;
+use Magento\Eav\Model\Entity\Attribute\Backend\AbstractBackend;
+use Magento\Framework\Exception\LocalizedException;
+
+class Material extends AbstractBackend
+{
+    /**
+    * @param Product @object
+    * @throws LocalizedException
+    */
+    public function validate($object)
+    {
+        $value = $object->getData($this->getAttribute()->getAttributeCode());
+        //TO DO - get the correct id and enter in the line below
+        if(($object->getAttributeSetId()) && ($value == 'fur')){
+            throw new LocalizedException(__('Bottoms cannot be fur'));
+        }
+    }
+}
---
+++ src/app/code/AdiDev/ProductAttribute/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'AdiDev_ProductAttribute',
+    __DIR__);
+

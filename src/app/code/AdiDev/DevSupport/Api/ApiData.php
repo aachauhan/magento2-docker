57b8739d7c5b8dadd654c80bb63a592caee76298
Aditya Chauhan <chauhan.adie@gmail.com>
2024-03-20 14:44:46 -0700

    Debugging API connection

---
+++ src/app/code/AdiDev/DevSupport/Api/ApiData.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace AdiDev\DevSupport\Api;
+
+use Magento\Framework\View\Element\Template;
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientFactory;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\ResponseFactory;
+use Magento\Framework\Webapi\Rest\Request;
+
+class ApiData extends \Magento\Framework\View\Element\Template
+{
+     /**
+     * API request URL
+     */
+    const API_REQUEST_URI = 'https://official-joke-api.appspot.com/random_joke';
+
+
+    /**
+     * @var ResponseFactory
+     */
+    private $responseFactory;
+
+    /**
+     * @var ClientFactory
+     */
+    private $clientFactory;
+
+    /**
+     * @var Curl
+     */
+    private $curlClient;
+
+
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        ClientFactory $clientFactory,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->clientFactory = $clientFactory;
+    }
+
+    /**
+     * Get API data
+     *
+     * @return array
+     */
+    public function execute(): array
+    {
+        $response = $this->doRequest(self::API_REQUEST_URI);
+        var_dump($response);
+        $statusCode = $response->getStatusCode();
+        $responseBody = $response->getBody();
+        $responseContent = $responseBody->getContents();
+
+        return json_decode($responseContent, true);
+
+    }
+
+    private function doRequest(
+        string $uriEndpoint,
+        array $params = [],
+        string $requestMethod = Request::HTTP_METHOD_GET
+    ) : Response {
+        $client = $this->clientFactory->create(['config' => [
+            'base_uri' => self::API_REQUEST_URI
+        ]]);
+        try{
+            $response = $client->request(
+                $requestMethod,
+                $uriEndpoint,
+                $params
+            );
+        } catch (GuzzleException $e) {
+            /** @var Response $response */
+            $response = $this->responseFactory->create([
+                'status' => $exception->getCode(),
+                'reason' => $exception->getMessage()
+            ]);
+        }
+        return $response;
+    }
+}
--- src/app/code/AdiDev/DevSupport/Block/Data.php
+++ src/app/code/AdiDev/DevSupport/Block/Data.php
@@ -3,44 +3,21 @@
 namespace AdiDev\DevSupport\Block;
 
 use Magento\Framework\View\Element\Template;
-use GuzzleHttp\Client;
-use GuzzleHttp\ClientFactory;
-use GuzzleHttp\Exception\GuzzleException;
-use GuzzleHttp\Psr7\Response;
-use GuzzleHttp\Psr7\ResponseFactory;
-use Magento\Framework\Webapi\Rest\Request;
+use AdiDev\DevSupport\Api\ApiData;
+
 
 class Data extends \Magento\Framework\View\Element\Template
 {
-     /**
-     * API request URL
-     */
-    const API_REQUEST_URI = 'https://official-joke-api.appspot.com/random_joke';
-
-
-    /**
-     * @var ResponseFactory
-     */
-    private $responseFactory;
-
-    /**
-     * @var ClientFactory
-     */
-    private $clientFactory;
-
-    /**
-     * @var Curl
-     */
-    private $curlClient;
+    protected $apiData;
 
 
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
-        ClientFactory $httpClientFactory,
+        \AdiDev\DevSupport\Api\ApiData $apiData,
         array $data = []
     ) {
+        $this->apiData = $apiData;
         parent::__construct($context, $data);
-        $this->httpClientFactory = $httpClientFactory;
     }
 
     /**
@@ -50,40 +27,15 @@ class Data extends \Magento\Framework\View\Element\Template
      */
     public function getAPIData(): array
     {
-        $response = $this->doRequest(self::API_REQUEST_URI);
-        $statusCode = $response->getStatusCode();
-        $responseBody = $response->getBody();
-        $responseContent = $responseBody->getContents();
-
-        return json_decode($responseContent, true);
-
+        $sample_joke = [
+            "type" => "general",
+            "setup" => "What kind of award did the dentist receive?",
+            "punchline" => "A little plaque.",
+            "id" => 255
+        ];
+        return $sample_joke;
+        //return $this->apiData->execute();
     }
 
-    private function doRequest(
-        string $uriEndpoint,
-        array $params = [],
-        string $requestMethod = Request::HTTP_METHOD_GET
-    ) : Response {
-        $client = $this->clientFactory->create(['config' => [
-            'base_uri' => self::API_REQUEST_URI
-        ]]);
-        try{
-            $response = $client->request(
-                $requestMethod,
-                $uriEndpoint,
-                $params
-            );
-        } catch (GuzzleException $e) {
-            /** @var Response $response */
-            $response = $this->responseFactory->create([
-                'status' => $exception->getCode(),
-                'reason' => $exception->getMessage()
-            ]);
-        }
-        return $response;
-    }
 
-    public function getTestFunction() {
-        return "Hello World";
-    }
 }
